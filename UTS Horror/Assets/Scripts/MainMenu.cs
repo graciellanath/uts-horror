2edd9fca1a56c804e350b94e5de369b3f2014dc1
graciellanath <graciellanathania8@gmail.com>
2025-10-15 21:09:17 +0700

    xc xds

--- UTS Horror/Assets/Scripts/MainMenu.cs
+++ UTS Horror/Assets/Scripts/MainMenu.cs
@@ -3,11 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Tombol Play
     public void PlayGame()
     {
         SceneManager.LoadScene("Map_Hosp1"); // Ganti dengan nama scene gameplay kamu
     }
 
+    // Tombol How To Play
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay"); // Pastikan scene HowToPlay sudah ditambahkan di Build Settings
+    }
+
+    // Tombol Quit
     public void QuitGame()
     {
         Application.Quit();
