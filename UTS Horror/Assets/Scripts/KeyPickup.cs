bd8cf6ebd6ec7f23e3944ad683cb6f799b1c0dea
graciellanath <graciellanathania8@gmail.com>
2025-10-15 15:48:35 +0700

    rifuaehuihfezsfnbs

--- UTS Horror/Assets/Scripts/KeyPickup.cs
+++ UTS Horror/Assets/Scripts/KeyPickup.cs
@@ -3,14 +3,36 @@ using UnityEngine;
 public class KeyPickup : MonoBehaviour
 {
     public DoorController doorController;
+    public Transform player;
+    public float interactDistance = 3f; // jarak interaksi
+    private bool isNear = false;
 
-    void OnTriggerEnter(Collider other)
+    void Update()
     {
-        if (other.CompareTag("Player"))
+        // Cek jarak antara player dan kunci
+        if (Vector3.Distance(player.position, transform.position) < interactDistance)
+        {
+            isNear = true;
+
+            // Tekan E untuk ambil kunci
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PickUpKey();
+            }
+        }
+        else
+        {
+            isNear = false;
+        }
+    }
+
+    void PickUpKey()
+    {
+        if (doorController != null)
         {
             doorController.playerHasKey = true;
             Debug.Log("Kunci diambil!");
-            Destroy(gameObject); // hilangkan kunci dari scene
+            Destroy(gameObject); // hapus kunci dari scene
         }
     }
 }
