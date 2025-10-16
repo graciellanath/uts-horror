e61cd06a6f31667e20be9a5710dbb92279a2adf7
KelvinSutadi <kelvinsutadi06@gmail.com>
2025-10-16 19:29:31 +0700

    benerin bug player

--- UTS Horror/Assets/Scripts/PlayerHasKey.cs
+++ UTS Horror/Assets/Scripts/PlayerHasKey.cs
@@ -3,4 +3,10 @@ using UnityEngine;
 public class PlayerHasKey : MonoBehaviour
 {
     public static bool hasKey = false;
+    private void Awake()
+    {
+        // setiap game baru dimulai, reset status kunci
+        hasKey = false;
+        Debug.Log("Status kunci di-reset ke false oleh Awake().");
+    }
 }
